546676d3480e8402d2d7ac6d6b3d1d2919ea4b71
David Price <david@mortware.net>
2020-03-31 12:06:50 +0100

    Added Rule extensions

---
+++ Opus/Validation/RuleBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using System.Collections.Generic;
+
+namespace Opus.Validation
+{
+    public static class RuleBuilderExtensions
+    {
+        public static IRuleBuilderOptions<T, IList<TElement>> MaxItems<T, TElement>(this IRuleBuilder<T, IList<TElement>> ruleBuilder, int max)
+        {
+            return ruleBuilder.Must((rootObject, list, context) =>
+            {
+                context.MessageFormatter
+                    .AppendArgument("MaxElements", max)
+                    .AppendArgument("TotalElements", list.Count);
+
+                return list.Count <= max;
+            })
+                .WithMessage("{PropertyName} must not contain more than {MaxElements} items. The list contains {TotalElements} items.");
+        }
+
+        public static IRuleBuilderOptions<T, bool?> BoolMustBeValue<T>(this IRuleBuilder<T, bool?> ruleBuilder, bool value)
+        {
+            return ruleBuilder.Must((rootObject, current, context) => current.HasValue && current.Value == value);
+        }
+    }
+}
